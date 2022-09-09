edc476fbfd2e9940af2f37e472ba48e80770c32f
Shamil Omarov <s_omarov05@mail.ru>
2022-09-09 05:15:54 +0300

    added a members to the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Employees from './components/Employees/Employees';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Team Member Allocation</h1>
+      <Header />
+      <Employees />
+      <Footer />
     </div>
   );
 }
